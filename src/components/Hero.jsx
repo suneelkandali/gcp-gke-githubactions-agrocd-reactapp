3f225f0dcc0c3dfcaf055f92cce35eb913e88cbe
sunkandali <suneelr.kandali@gmail.com>
2026-06-05 07:35:07 -0500

    Added changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Hero() {
   return (
     <div className="hero-container">
-      <h1>🚀 React App Successfully Deployed! -- Deployed with GitHub actions ...on June 5th 2026</h1>
+      <h1>🚀 React App Successfully Deployed! -- Deployed with GitHub actions ...on June 5th 2026 Suneel</h1>
       <p>Powered by <strong>GitHub Actions</strong> (CI) & <strong>ArgoCD</strong> (CD) running on <strong>GKE</strong>.</p>
       <div className="status-badge">
         <span className="dot"></span> GitOps Sync: Active
